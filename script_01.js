4e00544008fa97eee1c7a7058294593a71bd40e0
Peter <schmidt.multimedia@gmail.com>
2021-08-05 15:02:55 +0200

    02/ Objekte + Hierarchien (Nested Objects)

--- script_01.js
+++ script_01.js
@@ -15,21 +15,46 @@
 let person = 
 {
         firstName:"Maurice",
+        // firstName:prompt("?"),
         familyName:"König",
-        salary:[12000,16000],
-        hasPermission: true
+        salary:[120000,160000],
+        hasPermission: true,
+        sayHello: 
+                function(){
+                        return "Hi, ich bin " + this.familyName + "!";
+                }
 };
 
-ausgabe(person);
-ausgabe(person.firstName);
-ausgabe(person.familyName);
-ausgabe(person.salary[0]);
-ausgabe(person.hasPermission);
+// ausgabe(person);
+// ausgabe(person.firstName);
+// ausgabe(person.familyName);
+// ausgabe(person.salary[0]);
+// ausgabe(person.hasPermission);
+// ausgabe(person.sayHello());
 
 const txt =     "Ich bin " + person.firstName + " " +
                 person.familyName + " und verdiene " +
                 person.salary[1] + " p.a."
-ausgabe(txt);
+// ausgabe(txt);
+
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest 
+
+let baikal = {
+                wert:"10m",
+                deep:{
+                        deeper:{
+                                deepest:"Das Licht - auf 1642m!"
+                        }
+                }
+}
+
+ausgabe(baikal.wert);
+ausgabe(baikal.deep.deeper.deepest); // 192.188.0.1
+
 
 
 /** Ausgabe */
